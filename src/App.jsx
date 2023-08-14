162b877cea7a42bbc4e82dee88e7922a17aac742
AbdallaMon <abdotlos60@gmail.com>
2023-08-14 21:24:10 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
-import { fetchData } from "./store/fetchData/fetchData";
+// import { fetchData } from "./store/fetchData/fetchData";
 import { useEffect } from "react";
 function App() {
   const data = useSelector((state) => state.data);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchData());
-  }, []);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchData());
+  // }, []);
   console.log(data);
   return (
-    <>
-      <img src={data.dataArr.obj[0].img} />
-    </>
+    <>{data.loading === false && <img src={data.dataArr.obj[0].img || ""} />}</>
   );
 }
 
--- src/store/fetchData/fetchData.js
+++ src/store/fetchData/fetchData.js
@@ -1,36 +1,36 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // import data from "../../product.json";
 import axios from "axios";
-
+import data from "../../db.json";
 const initialState = {
   loading: false,
   error: "",
-  dataArr: [],
+  dataArr: data,
 };
 
-export const fetchData = createAsyncThunk("data/fetchData", () => {
-  return axios.get("../../../db.json").then((res) => res.data);
-});
+// export const fetchData = createAsyncThunk("data/fetchData", () => {
+//   return axios.get(data).then((res) => res.data);
+// });
 
 const userSlice = createSlice({
   name: "data",
   initialState,
-  extraReducers: (builder) => {
-    builder.addCase(fetchData.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(fetchData.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = "";
-      state.dataArr = action.payload;
-    });
-    builder.addCase(fetchData.rejected, (state, action) => {
-      state.loading = false;
-      state.dataArr = "";
-      state.error = action.error.message;
-    });
-  },
+  // extraReducers: (builder) => {
+  //   builder.addCase(fetchData.pending, (state) => {
+  //     state.loading = true;
+  //   });
+  //   builder.addCase(fetchData.fulfilled, (state, action) => {
+  //     state.loading = false;
+  //     state.error = "";
+  //     state.dataArr = action.payload;
+  //   });
+  //   builder.addCase(fetchData.rejected, (state, action) => {
+  //     state.loading = false;
+  //     state.dataArr = "";
+  //     state.error = action.error.message;
+  //   });
+  // },
 });
 export default userSlice.reducer;
-export const { filterByCat, sliceData, filterByPrice, sort, handleCart } =
-  userSlice.actions;
+// export const { filterByCat, sliceData, filterByPrice, sort, handleCart } =
+//   userSlice.actions;
